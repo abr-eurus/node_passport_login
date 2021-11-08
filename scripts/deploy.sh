1d4d1544c7086cb1e9b452682280fc7c1abdd0d5
abr-eurus <ammar.bin.rizwan@eurustechnologies.com>
2021-11-08 17:38:31 +0500

    deploy script updatedd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -avz -e "ssh -i /home/ubuntu/ammar-KeyPair.pem" ./ ubuntu@107.23.15.131:/var/www/html
-#ssh -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@107.23.15.131 '/var/www/html/install_dependencies.sh'
-ssh -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@107.23.15.131 'sudo systemctl restart nodeapp'
+rsync -avz -e "ssh -i /home/ubuntu/ammar-KeyPair.pem" ./ ubuntu@54.164.94.109:/var/www/html
+#ssh -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@54.164.94.109 '/var/www/html/install_dependencies.sh'
+ssh -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@54.164.94.109 'sudo systemctl restart nodeapp'
