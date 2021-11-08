ba4b9c114c78aa04b2ab593898ffb2d067631bbe
abr-eurus <ammar.bin.rizwan@eurustechnologies.com>
2021-11-08 18:19:07 +0500

    default password addedd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo 48f31904357c48daa85b7888f51413e5 | sudo -S rsync -avz -e "ssh -i /home/ubuntu/ammar-KeyPair.pem" ./ ubuntu@54.164.94.109:/var/www/html
+rsync -avz -e "ssh -i /home/ubuntu/ammar-KeyPair.pem" ./ ubuntu@54.164.94.109:/var/www/html
 #ssh -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@54.164.94.109 '/var/www/html/install_dependencies.sh'
 ssh -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@54.164.94.109 'sudo systemctl restart nodeapp'
