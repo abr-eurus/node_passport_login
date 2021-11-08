16bf3685cd361a1f812e667d188f22db02b87279
abr-eurus <ammar.bin.rizwan@eurustechnologies.com>
2021-11-08 16:50:56 +0500

    path corrected

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rsync -avz -e "ssh -i /home/ubuntu/ammar-KeyPair.pem" ./ ubuntu@107.23.15.131:/var/www/html
-ssh -i ammar-KeyPair.pem  ubuntu@107.23.15.131 'touch /var/www/html/install_dependencies.sh'
-ssh -i ammar-KeyPair.pem  ubuntu@107.23.15.131 'systemctl restart nodeapp'
+ssh -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@107.23.15.131 'touch /var/www/html/install_dependencies.sh'
+ssh -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@107.23.15.131 'systemctl restart nodeapp'
