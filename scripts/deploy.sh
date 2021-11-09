c6bea513ba593bc4a246372b7a89a03af9919e1c
abr-eurus <ammar.bin.rizwan@eurustechnologies.com>
2021-11-09 16:18:27 +0500

    sudo added

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 HOST_NAME= ubuntu@$1
 
-rsync -avz -e "sudo ssh -o StrictHostKeyChecking=no -i /home/ubuntu/ammar-KeyPair.pem" ./ $HOST_NAME:/var/www/html
+sudo rsync -avz -e "sudo ssh -o StrictHostKeyChecking=no -i /home/ubuntu/ammar-KeyPair.pem" ./ $HOST_NAME:/var/www/html
 sudo ssh  -o StrictHostKeyChecking=no -i /home/ubuntu/ammar-KeyPair.pem  $HOST_NAME 'sudo systemctl restart nodeapp'
