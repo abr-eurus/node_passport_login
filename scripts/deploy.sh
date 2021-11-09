d6dd0c18e2a6db71451309c4208408653be8aa8c
abr-eurus <ammar.bin.rizwan@eurustechnologies.com>
2021-11-09 14:18:21 +0500

    --rsync-path removed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 URL_NODE_APP=54.221.132.176
 
-rsync --rsync-path='sudo rsync' -avz -e "sudo ssh -o StrictHostKeyChecking=no -i /home/ubuntu/ammar-KeyPair.pem" ./ ubuntu@$URL_NODE_APP:/var/www/html
+sudo rsync -avz -e "sudo ssh -o StrictHostKeyChecking=no -i /home/ubuntu/ammar-KeyPair.pem" ./ ubuntu@$URL_NODE_APP:/var/www/html
 sudo ssh  -o StrictHostKeyChecking=no -i /home/ubuntu/ammar-KeyPair.pem  ubuntu@$URL_NODE_APP 'sudo systemctl restart nodeapp'
